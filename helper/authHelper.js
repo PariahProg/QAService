548e4e0e02528d98dd747b22e31fe318cea62296
PariahProg <pariah.prog@gmail.com>
2020-06-10 02:00:38 +0500

    authHelper complemented, middlewares for authentication and verifying added

--- helper/authHelper.js
+++ helper/authHelper.js
@@ -1,3 +1,45 @@
+
+const {secret, tokens} = require('../config/app').jwt;
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
+const Token = require('../models/token');
+
+
+exports.generateAccessToken = (userId) => {
+    const payload = {
+        //id: uuidv4(),
+        userId: userId,
+        type: tokens.access.type,
+    };
+    const options = {expiresIn: tokens.access.expiresIn};
+
+    return jwt.sign(payload, secret, options);
+}
+
+exports.generateRefreshToken = () => {
+    const payload = {
+        id: uuidv4(),
+        type: tokens.refresh.type,
+    };
+
+    const options = {expiresIn: tokens.refresh.expiresIn};
+
+    return {
+        id: payload.id,
+        token: jwt.sign(payload, secret, options),
+    }
+}
+
+exports.replaceDbRefreshToken = async (tokenId, userId) => {
+    await Token.delete(userId);
+    await Token.create(tokenId, userId);
+}
+
+exports.isEmail = email => {
+    const isEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return isEmail.test(email);
+}
+
 exports.isUsername = username => {
     const isUsername = /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
     return isUsername.test(username);
