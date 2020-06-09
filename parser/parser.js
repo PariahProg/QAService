f32610db30865c906cd7a65bce2cf498929dbed3
Overloque <poludnicyn.99@mail.ru>
2020-06-10 02:36:36 +0500

    Added parsing companies in parser.js

--- parser/parser.js
+++ parser/parser.js
@@ -3,8 +3,10 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 let urlQuestions = 'https://stackoverflow.com/questions';
+let urlCompanies = 'https://stackoverflow.com/jobs/companies';
 
 let dataQuestions = [];
+let dataCompanies = [];
 
 let indexPage = 1;
 
@@ -38,9 +40,43 @@ function getQuestions(URL, indexPage) {
     })
 }
 
+function getCompanies(URL, indexPage) {
+    needle.get(URL, (err, res) => {
+        if (err) throw err;
+        while (indexPage < 10) {
+            let $ = cheerio.load(res.body);
+    
+            $('.dismissable-company.-company.ps-relative.js-dismiss-overlay-container.p24.bb.bc-black-3').each((index, element) => {
+                const company = $(element).find('.fs-body2.mb4 > a').text().replace(/\n/g, '').replace(/s\s+/, '').trim();
+                const location = $(element).find('.grid--cell.fc-black-500.fs-body1').text().trim();
+                const companyDescription = $(element).find('.mt8.mb0.fs-body1.fc-black-700').text().replace(/\n/g, '').replace(/s\s+/, '').trim();
+
+                dataCompanies.push({
+                    company: company,
+                    information: location,
+                    companyDescription: companyDescription,
+                    page: indexPage
+                });
+            })
+
+            writeFileCompanies(fs);
+            indexPage++;
+            URL = `https://stackoverflow.com/jobs/companies?pg=${indexPage}`;
+    
+            return getCompanies(URL, indexPage);
+        }
+    })
+}
+
+
+function writeFileCompanies(fs) {
+    return fs.appendFileSync('./data.json', JSON.stringify(dataCompanies, null, 4));
+}
+
 function writeFileQuestions(fs) {   
     return fs.writeFileSync('./data.json', JSON.stringify(dataQuestions, null, 4));
 }
 
 getQuestions(urlQuestions, indexPage);
 
+getCompanies(urlCompanies, indexPage);
